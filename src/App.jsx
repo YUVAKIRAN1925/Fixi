69a69f5b5b5443554911ba765ae60f3f406d309c
YUVA KIRAN <yuvakiran1551@gmail.com>
2024-06-01 12:38:29 +0530

    'second commit'

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,8 @@ function App() {
           <Route exact path='/partners' element={<HowItWorks />}/>
           <Route exact path='/terms-and-conditions' element={<TermsAndConditions />}/>
           <Route exact path='/privacy-policy' element={<PrivacyPolicy />}/>
+
+          <div>hi kiran</div>
         </Routes>
         <Footer />
       </Router>
